3b4669f473160bb03d48f66da979503d4df2899f
Havard E <havarde.kodehode@gmail.com>
2023-05-23 10:18:31 +0200

    refactored, add and clear functions in its own js

---
+++ functions.js
@@ -0,0 +1,7 @@
+export function clear(elementToClear){
+   elementToClear.value = ""
+}
+
+export function addWare(ulElement, ware){
+    ulElement.innerHTML += `<li>${ware}</li>`
+}
--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { clear, addWare } from "./functions.js"
 
 const appSettings = {databaseURL: "https://realtime-database-dc353-default-rtdb.europe-west1.firebasedatabase.app/"}
 
 const app = initializeApp(appSettings) //Connecting firebase to this project
 const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
-const shoppingListInDB = ref(database, "shoppingList")
+const ulEl = document.getElementById("shopping-list")
+
+let wares = []
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
 
     push(shoppingListInDB, inputValue)
+
+    //ulEl.innerHTML += `<li>${inputValue}</li>`
+    addWare(ulEl, inputValue)
     
     console.log(inputValue)
+
+    // inputFieldEl.value = ""
+    clear(inputFieldEl)
+
+
+
 })
