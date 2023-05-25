019ec91556ba6e5fc1a2e4dfc06fd57b34428fba
Havard E <havarde.kodehode@gmail.com>
2023-05-25 14:13:28 +0200

    Added option to retrieve hardcoded elements to the database

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 import { clear, addWare } from "./functions.js"
 
-const appSettings = {databaseURL: "https://realtime-database-dc353-default-rtdb.europe-west1.firebasedatabase.app/"}
+const appSettings = {databaseURL: "https://playground-2c556-default-rtdb.europe-west1.firebasedatabase.app/"}
 
 const app = initializeApp(appSettings) //Connecting firebase to this project
 const database = getDatabase(app)
@@ -15,7 +15,20 @@ const ulEl = document.getElementById("shopping-list")
 
 let wares = []
 
+
+//Note to self: Learn JQuery
+
+// document.getElementById('items').getElementsByTagName('li').length >= 1
+// With JQuery: $('ul#items li').length >= 1
+
+//Adding hardcoded list items
+// let items = ulEl.querySelectorAll("li")
+// items.forEach(item => {
+//
+// });
+
 addButtonEl.addEventListener("click", function() {
+
     let inputValue = inputFieldEl.value
 
     push(shoppingListInDB, inputValue)
@@ -23,11 +36,13 @@ addButtonEl.addEventListener("click", function() {
     //ulEl.innerHTML += `<li>${inputValue}</li>`
     addWare(ulEl, inputValue)
     
-    console.log(inputValue)
-
     // inputFieldEl.value = ""
     clear(inputFieldEl)
+})
 
+onValue(shoppingListInDB, function(snapshot){
+    // itemsArray = Object.values(snapshot.val())
 
+    console.log(snapshot.val())
 
 })
