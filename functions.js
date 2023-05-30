9acf4a98a627d199dc0347c11528e4df824b8bfd
Havard E <havarde.kodehode@gmail.com>
2023-05-30 14:13:32 +0200

    Added delete on dblClick functionality

--- functions.js
+++ functions.js
@@ -1,9 +1,32 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {databaseURL: "https://playground-2c556-default-rtdb.europe-west1.firebasedatabase.app/"}
+
+const app = initializeApp(appSettings) //Connecting firebase to this project
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 export function clear(elementToClear){
    elementToClear.value = ""
 }
 
 export function addWare(ulElement, ware){
-    ulElement.innerHTML += `<li>${ware}</li>`
+    // ulElement.innerHTML += `<li>${ware}</li>`
+    let wareID = ware[0]
+    const li = document.createElement("li")
+    li.textContent = ware[1]
+
+    li.addEventListener("dblclick", function(){
+        let wareToDelete = ref(database, `shoppingList/${wareID}`) 
+        remove(wareToDelete)
+    })
+
+    ulElement.append(li)
+}
+
+export function clearShoppingListEl(ulEl){
+    ulEl.innerHTML = ""
 }
 
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-import { clear, addWare } from "./functions.js"
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { clear, addWare, clearShoppingListEl } from "./functions.js"
 
 const appSettings = {databaseURL: "https://playground-2c556-default-rtdb.europe-west1.firebasedatabase.app/"}
 
@@ -20,18 +20,44 @@ addButtonEl.addEventListener("click", function() {
     clear(inputFieldEl)
 })
 
+
+// const myOnValue = 
 onValue(shoppingListInDB, function(snapshot){
-    let itemsArray = Object.values(snapshot.val())
+    if(snapshot.exists()){
+        let itemsArray = Object.entries(snapshot.val())
 
-    ulEl.innerHTML = ""
-
-    console.log(snapshot.val())
-
-
-    for (let i = 0; i < itemsArray.length; i++) {
-        let item = itemsArray[i];
-        console.log(item);
-        addWare(ulEl,item)
+        clearShoppingListEl(ulEl)
+        ulEl.innerHTML = ""
+    
+        console.log(snapshot.val())
+    
+    
+        for (let i = 0; i < itemsArray.length; i++) {
+            let item = itemsArray[i];
+            let itemID = item[0]
+            let itemValue = item[1]
+            console.log(itemID);
+            addWare(ulEl,item)
+        }
+    }else{
+        ulEl.innerHTML = "No items yet"
     }
+   
 
 })
+
+
+
+//Promise Test
+
+// let listHasElements = new Promise(function(resolve, reject){
+//     if(!Object.entries(snapshot.val()) === null){
+//         resolve()
+//     }else{
+//         reject("")
+//     }
+   
+// })
+
+
+
